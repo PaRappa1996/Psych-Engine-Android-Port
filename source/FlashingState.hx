61057709fe69af60282c62fedacfa8f47b2d4365
The SillySylv <104529160+RenTheSylvee@users.noreply.github.com>
2023-01-09 02:42:51 -0300

    bru-

--- source/FlashingState.hx
+++ source/FlashingState.hx
@@ -24,11 +24,11 @@ class FlashingState extends MusicBeatState
 		add(bg);
 
 		warnText = new FlxText(0, 0, FlxG.width,
-			"Hey, watch out!\n
-			This Mod contains some flashing lights!\n
-			Press ENTER to disable them now or go to Options Menu.\n
-			Press ESCAPE to ignore this message.\n
-			You've been warned!",
+			"Quieto Parao!\n
+			Puedes romper la pantalla si la tocas fuerte!\n
+			Ademas este mod tiene luces flash.\n
+			Pulsa A para desactivarlas o B para ignorar el mensaje.\n
+			Te lo adverti capo!",
 			32);
 		warnText.setFormat("VCR OSD Mono", 32, FlxColor.WHITE, CENTER);
 		warnText.screenCenter(Y);
